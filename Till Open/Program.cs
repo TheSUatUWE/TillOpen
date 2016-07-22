a0856e09c10d32fc867eff15d616ae5a8590f185
Matt Ribbins <matt@mattyribbo.co.uk>
2016-07-22 10:32:31 +0100

    Initial commit
    Adding to source control

---
+++ Till Open/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO.Ports;
+using System.Collections.Specialized;
+using System.Configuration;
+
+public class TillOpen
+{
+    static SerialPort _serialPort;
+
+    public static void Main()
+    {
+        NameValueCollection settings = ConfigurationManager.GetSection("SU.appSettings") as NameValueCollection;
+        string message;
+        string port;
+
+        // Create a new SerialPort object with default settings.
+        _serialPort = new SerialPort();
+        message = settings["Message"];
+        int iMsg = Int32.Parse(message, System.Globalization.NumberStyles.HexNumber);
+        message = "" + (char) iMsg;
+
+        port = settings["Port"];
+        _serialPort.PortName = port;
+
+        // Set the read/write timeouts
+        _serialPort.ReadTimeout = 500;
+        _serialPort.WriteTimeout = 500;
+
+        _serialPort.Open();
+
+        _serialPort.WriteLine(message);
+
+        _serialPort.Close();
+    }
+
+    // Display Port values and prompt user to enter a port. 
+    public static string SetPortName()
+    {
+        string portName = "COM1";
+
+        Console.WriteLine("Available Ports:");
+        foreach (string s in SerialPort.GetPortNames())
+        {
+            portName = s;
+            break;
+        }
+
+        return portName;
+    }
+}
+
